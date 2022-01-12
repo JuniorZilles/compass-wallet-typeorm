c803e19cdf243cfbc7cc1c14f0711e3a3ccb0663
Junior Zilles <junior_zilles@hotmail.com>
2022-01-12 14:08:07 -0300

    feat: include wallet entity

--- src/wallet/entities/wallet.entity.ts
+++ src/wallet/entities/wallet.entity.ts
@@ -1,1 +1,29 @@
-export default class Wallet {}
+import { Exclude } from 'class-transformer';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('wallet')
+export default class Wallet {
+  @PrimaryGeneratedColumn('uuid')
+  address: string;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ nullable: false, unique: true })
+  cpf: string;
+
+  @Column({ nullable: false })
+  birthdate: Date;
+
+  @Exclude()
+  @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP(6)', select: false })
+  created_at: Date;
+
+  @Exclude()
+  @UpdateDateColumn({
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+    select: false
+  })
+  updated_at: Date;
+}
