77e1d8c51ae22878c7457f886867b077871f5dfe
Junior Zilles <junior_zilles@hotmail.com>
2022-01-12 16:43:02 -0300

    ref: remove methods that wil be not used

--- src/wallet/transaction/transaction.controller.ts
+++ src/wallet/transaction/transaction.controller.ts
@@ -32,19 +32,4 @@ export default class TransactionController {
   findAll() {
     return this.transactionService.findAll();
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
-  }
 }
--- src/wallet/transaction/transaction.service.ts
+++ src/wallet/transaction/transaction.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import CreateTransactionDto from './dto/create-transaction.dto';
-import UpdateTransactionDto from './dto/update-transaction.dto';
 
 @Injectable()
 export default class TransactionService {
@@ -10,17 +9,5 @@ export default class TransactionService {
 
   findAll() {
     return `This action returns all transaction`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
-
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
   }
 }
