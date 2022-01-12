fef55e35b382b42e8ff5cbd484a4c20712c35e5b
Junior Zilles <junior_zilles@hotmail.com>
2022-01-12 13:22:11 -0300

    feat: insert transaction module

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TransactionModule } from './transaction/transaction.module';
 import WalletService from './wallet.service';
 import WalletController from './wallet.controller';
 
 @Module({
   controllers: [WalletController],
-  providers: [WalletService]
+  providers: [WalletService],
+  imports: [TransactionModule]
 })
 export default class WalletModule {}
