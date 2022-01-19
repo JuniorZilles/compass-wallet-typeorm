bb6cc93a0b0af1c926fbd45e5d348ed38950c52a
Junior Zilles <junior_zilles@hotmail.com>
2022-01-19 15:31:33 -0300

    feat: basic service and controller test

--- test/unit/wallet/transaction/transaction.controller.spec.ts
+++ test/unit/wallet/transaction/transaction.controller.spec.ts
@@ -2,19 +2,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import TransactionController from '../../../../src/wallet/transaction/transaction.controller';
 import TransactionService from '../../../../src/wallet/transaction/transaction.service';
 
-describe('TransactionController', () => {
-  let controller: TransactionController;
+describe('scr :: api :: wallet :: transaction :: WalletController()', () => {
+  describe('GIVEN a context that TransactionModule is not instantiated', () => {
+    describe('WHEN the TransactionModule is instantiated', () => {
+      let controller: TransactionController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TransactionController],
-      providers: [TransactionService]
-    }).compile();
+      beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          controllers: [TransactionController],
+          providers: [TransactionService]
+        }).compile();
 
-    controller = module.get<TransactionController>(TransactionController);
-  });
+        controller = module.get<TransactionController>(TransactionController);
+      });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+      it('THEN the WalletController should be defined', () => {
+        expect(controller).toBeDefined();
+      });
+
+      it('THEN the WalletController should have a findAll method', () => {
+        expect(controller).toHaveProperty('findAll');
+      });
+
+      it('THEN the WalletController should have a create method', () => {
+        expect(controller).toHaveProperty('create');
+      });
+    });
   });
 });
--- test/unit/wallet/transaction/transaction.service.spec.ts
+++ test/unit/wallet/transaction/transaction.service.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import TransactionService from '../../../../src/wallet/transaction/transaction.service';
 
-describe('TransactionService', () => {
-  let service: TransactionService;
+describe('scr :: api :: wallet :: transaction :: TransactionService()', () => {
+  describe('GIVEN a context that a Module is not instantiated', () => {
+    describe('WHEN the Module is instantiated just with the provider TransactionService', () => {
+      let service: TransactionService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionService]
-    }).compile();
+      beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+          providers: [TransactionService]
+        }).compile();
 
-    service = module.get<TransactionService>(TransactionService);
-  });
+        service = module.get<TransactionService>(TransactionService);
+      });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+      it('THEN the TransactionService should be defined', () => {
+        expect(service).toBeDefined();
+      });
+
+      it('THEN the TransactionService should have a findAll method', () => {
+        expect(service).toHaveProperty('findAll');
+      });
+
+      it('THEN the TransactionService should have a findOne method', () => {
+        expect(service).toHaveProperty('create');
+      });
+    });
   });
 });
